78e80730cf23ea7e19d4fee7926466a8f319eb3a
joschonarth <joschonarth@gmail.com>
2025-06-26 18:36:07 -0300

    chore: setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import path from "node:path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+/// <reference types="vitest" />
+
+import path from 'node:path'
+
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}) 
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+})
